fb4c14fd38c82f0932e06c36d0bb0886cc8055f7
Dreaming Huang <593997095@qq.com>
2024-07-01 16:34:22 +0800

    build

---
+++ include/stepposition.h
@@ -0,0 +1,16 @@
+class position
+{
+   public :
+   int cost;
+   position(){}
+   ~position(){}
+
+};
+class Shipposition:
+{
+   public :
+   int cost
+   position(){}
+   ~position(){}
+
+};
---
+++ index.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+   std::cout<<"hello world"<<std::endl;
+}
+
+struct socket
+{
+   int black=5;
+   int green=5;
+   int grey=5;
+   int blue=5;
+}
+enum player
+{
+    
+}
